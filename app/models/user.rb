2df7e49b208df5e9ca47a3f16e0ffa898f295aa1
veenaguruprasad <veena@bmsit.in>
2021-11-30 21:30:44 +0530

    Owner can see a list of customers

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ActiveRecord::Base
   has_many :order
   validates :name, presence: true
   validates :email, { presence: true, uniqueness: true }
+
+  def self.customers_only
+    where(user_type: "customer")
+  end
 end
